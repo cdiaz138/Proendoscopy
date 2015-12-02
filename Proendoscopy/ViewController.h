5bd351650a9f49273cbea656b78884381d1e1af1
Idelfonso Gutierrez <idelfonsong2@gmail.com>
2015-12-01 19:21:41 -0500

    Initial Commit

---
+++ Proendoscopy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Proendoscopy
+//
+//  Created by Idelfonso Gutierrez Jr. on 12/1/15.
+//  Copyright © 2015 FIU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProendoscopyTests/ProendoscopyTests.m
@@ -0,0 +1,39 @@
+//
+//  ProendoscopyTests.m
+//  ProendoscopyTests
+//
+//  Created by Idelfonso Gutierrez Jr. on 12/1/15.
+//  Copyright © 2015 FIU. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ProendoscopyTests : XCTestCase
+
+@end
+
+@implementation ProendoscopyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
